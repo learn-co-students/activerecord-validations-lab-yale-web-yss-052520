72f5d2bd57b8164d9c349f9a9043c3f756f64e53
nicolewang2000 <nicole.wang@yale.edu>
2020-06-23 16:36:20 -0400

    Done.

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,12 +1,7 @@
 class PostValidator < ActiveModel::Validator
     def validate(record)
-        if !record.title
-            record.errors[:title] << "Title Missing"
-        end
-        
         unless record.title.to_s.include?("Won't Believe" || "Secret" || "Top [number]" ||"Guess")
             record.errors[:title] << "Title is not sufficiently clickbait-y"
         end
-        
     end
 end
