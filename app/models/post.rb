0774cabe561a7d08dc776feeec61b5ffb8e03724
chad <chad.palmer@yale.edu>
2020-06-29 14:53:07 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validate :is_clickbait
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    def is_clickbait
+        if title
+            unless title.match(/Won't Believe/) || title.match(/Secret/) || title.match(/Top +[0-9]/) || title.match(/Guess/)
+                errors.add(:title, "not sufficiently clickbait-y")
+            end
+        end
+    end
 end
